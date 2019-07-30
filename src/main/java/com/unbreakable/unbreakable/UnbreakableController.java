73ab19221be632d59b934a6b1122c2494d67dd2e
Ojka91 <ojkalp@gmail.com>
2019-07-30 08:52:30 +0200

    full filter endpoint

--- src/main/java/com/unbreakable/unbreakable/UnbreakableController.java
+++ src/main/java/com/unbreakable/unbreakable/UnbreakableController.java
@@ -89,4 +89,52 @@ public class UnbreakableController {
 
     }
 
+
+    @RequestMapping("/{discipline}/{group}/{level}")
+    public List getEx(@PathVariable String discipline, @PathVariable List<String> group, @PathVariable List<Integer> level){
+        if(discipline.equals("calisthenics")){
+            if(!group.equals("all")){
+
+                return exercices_calisthenicsRepository.findAll()
+                        .stream()
+                        .filter(el->group.contains(el.getGroup()) && level.contains(el.getLevel()))
+                        .collect(Collectors.toList());
+            } else{
+                return exercices_calisthenicsRepository.findAll()
+                        .stream()
+                        .filter(el->level.contains(el.getLevel()))
+                        .collect(Collectors.toList());
+            }
+        }
+        if(discipline.equals("athletics")){
+            if(!group.equals("all")){
+
+                return exercises_athleticsRepository.findAll()
+                        .stream()
+                        .filter(el->group.contains(el.getGroup()) && level.contains(el.getLevel()))
+                        .collect(Collectors.toList());
+            } else{
+                return exercises_athleticsRepository.findAll()
+                        .stream()
+                        .filter(el->level.contains(el.getLevel()))
+                        .collect(Collectors.toList());
+            }
+        }
+        if(discipline.equals("parkour")){
+            if(!group.equals("all")){
+
+                return exercises_parkourRepository.findAll()
+                        .stream()
+                        .filter(el->group.contains(el.getGroup()) && level.contains(el.getLevel()))
+                        .collect(Collectors.toList());
+            } else{
+                return exercises_parkourRepository.findAll()
+                        .stream()
+                        .filter(el->level.contains(el.getLevel()))
+                        .collect(Collectors.toList());
+            }
+        }
+
+    }
+
 }
